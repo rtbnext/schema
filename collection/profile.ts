12b69c53ca244a01e1b5c017bda88d2399930ece
komed3 <pk97@outlook.de>
2025-12-02 00:06:01 +0100

    Update profile.ts

--- collection/profile.ts
+++ collection/profile.ts
@@ -2,6 +2,8 @@ import { BrandedProfile, ProfileURI } from '../abstract/primitives';
 
 export type Profile< P extends ProfileURI > = BrandedProfile< {}, P >;
 
-export type ProfileCollection = {
-    items: { [ P in ProfileURI ]: Profile< P > }
+export type ProfileCollection< P extends ProfileURI = ProfileURI > = {
+    index: { [ K in P ]: never };
+    alias: Record< string, P >;
+    items: { [ K in P ]: Profile< K > }
 };
