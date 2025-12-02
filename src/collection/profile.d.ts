0c52e19a8afcebb64590852fd5dd778a36a77fe7
komed3 <pk97@outlook.de>
2025-12-02 08:42:42 +0100

    profile info

--- src/collection/profile.d.ts
+++ src/collection/profile.d.ts
@@ -1,6 +1,40 @@
+import * as Const from '../abstract/const';
 import { BrandedProfile, ProfileURI } from '../abstract/primitives';
 
-export type Profile< P extends ProfileURI > = BrandedProfile< {}, P >;
+export type ProfileInfo< P extends ProfileURI > = BrandedProfile< {
+    readonly uri: P;
+    deceased: boolean;
+    family: boolean;
+    dropOff: boolean;
+    name: string;
+    shortName: string;
+    gender: Const.Gender;
+    birthDate?: string;
+    birthPlace?: Location;
+    citizenship: Const.ISOCountryCode;
+    residence: Location;
+    maritalStatus?: Const.MaritalStatus;
+    children?: number;
+    education?: {
+        school: string;
+        degree?: string;
+    }[];
+    industry: Const.Industry;
+    source: string[];
+    selfMade?: {
+        type: string;
+        is: boolean;
+        rank?: Const.SelfMadeRank;
+    };
+    organization?: {
+        name: string;
+        title?: string;
+    };
+}, P >;
+
+export type Profile< P extends ProfileURI > = {
+    info: ProfileInfo< P >;
+};
 
 export type ProfileCollection< P extends ProfileURI = ProfileURI > = {
     index: { [ K in P ]: never };
