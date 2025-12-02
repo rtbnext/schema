11a23311e45b4bd1dd2deec742b3962313238f89
komed3 <pk97@outlook.de>
2025-12-02 09:32:27 +0100

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,10 @@
+import { DateString } from './abstract/primitives';
+import * as Utils from './abstract/utils';
+
 import { ProfileCollection } from './collection/profile';
 
-export type BillionairesIndex = {
+export type BillionairesIndex = Utils.MetaData & {
+    currentDay: DateString;
+    availableDays: DateString[];
     profile: ProfileCollection;
 };
