b6201ee364102fcf8ca0db4a5a0e600df648e3cc
komed3 <pk97@outlook.de>
2025-12-02 10:02:28 +0100

    implement stats collection

--- src/collection/stats.d.ts
+++ src/collection/stats.d.ts
@@ -15,15 +15,26 @@ export type HistoryData = [
     ProfileURI
 ];
 
-export type Stats< T extends string > = {
-    [ K in T ]: {
-        list: ListItem;
-        history: HistoryData[];
-    };
+export type Stats< T extends string > = { [ K in T ]: {
+    list: ListItem;
+    history: HistoryData[];
+} };
+
+export type StatsList< T extends string > = { [ K in T ]: number };
+
+export type AgePyramid = {
+    [ G in Const.Gender ]: StatsList< '10' | '20' | '30' | '40' | '50' | '60' | '70' | '80' | '90' >
 };
 
 export type StatsCollection = Utils.MetaData & {
     industry: Stats< Const.Industry >;
     country: Stats< Const.ISOCountryCode >;
     state: Stats< Const.USStateCode >;
+    selfMade: StatsList< Const.SelfMadeRank >;
+    maritalStatus: StatsList< Const.MaritalStatus >;
+    agePyramid: AgePyramid;
+    children: {
+        full: StatsList< `${number}` >;
+        short: StatsList< 'none' | 'one' | 'two' | 'three' | 'four' | '5-to-10' | 'over-10' >;
+    };
 };
