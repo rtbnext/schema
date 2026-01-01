dbbeb504a9722bf8ff766ba8e12ee7c9829e2607
komed3 <pk97@outlook.de>
2026-01-01 17:42:54 +0100

    add TGenericStats type

---
+++ src/model/stats.d.ts
@@ -0,0 +1,9 @@
+import { TChange } from '../abstract/assets';
+
+export type TGenericStats = TChange & {
+    date: string;
+    count: number;
+    total: number;
+    woman: number;
+    quota: number;
+};
