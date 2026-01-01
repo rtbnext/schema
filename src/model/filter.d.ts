d62f32835c727ab5f7ffd3c1156a88605b7d2c3e
komed3 <pk97@outlook.de>
2026-01-01 17:31:38 +0100

    Create filter.d.ts

---
+++ src/model/filter.d.ts
@@ -0,0 +1,29 @@
+import { TGender, TMaritalStatus } from '../abstract/const';
+import { TMetaData } from '../abstract/generic';
+
+export interface TFilter {
+    readonly uri: string;
+    name: string;
+    value: unknown;
+}
+
+export type TFilterList = TMetaData & {
+    items: TFilter[];
+    count: number;
+};
+
+export interface TFilterCollection {
+    industry: Record< string, TFilter[] >;
+    citizenship: Record< string, TFilter[] >;
+    country: Record< string, TFilter[] >;
+    state: Record< string, TFilter[] >;
+    gender: { [ K in TGender ]?: TFilter[] };
+    age: Record< number, TFilter[] >;
+    maritalStatus: { [ K in TMaritalStatus ]?: TFilter[] };
+    special: {
+        deceased: TFilter[];
+        dropOff: TFilter[];
+        family: TFilter[];
+        selfMade: TFilter[];
+    };
+}
