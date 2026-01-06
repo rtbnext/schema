1bfaa833e91efee5e16f93132f91a7808639e246
komed3 <pk97@outlook.de>
2026-01-06 09:16:51 +0100

    Update filter.d.ts

--- src/model/filter.d.ts
+++ src/model/filter.d.ts
@@ -12,18 +12,34 @@ export type TFilter = TMetaData & {
     count: number;
 }
 
-export interface TFilterCollection {
-    industry: Record< string, TFilter[] >;
-    citizenship: Record< string, TFilter[] >;
-    country: Record< string, TFilter[] >;
-    state: Record< string, TFilter[] >;
-    gender: { [ K in TGender ]?: TFilter[] };
-    age: Record< number, TFilter[] >;
-    maritalStatus: { [ K in TMaritalStatus ]?: TFilter[] };
+export interface TFilterList {
+    industry: Record< string, TFilterItem[] >;
+    citizenship: Record< string, TFilterItem[] >;
+    country: Record< string, TFilterItem[] >;
+    state: Record< string, TFilterItem[] >;
+    gender: { [ K in TGender ]?: TFilterItem[] };
+    age: Record< number, TFilterItem[] >;
+    maritalStatus: { [ K in TMaritalStatus ]?: TFilterItem[] };
     special: {
-        deceased: TFilter[];
-        dropOff: TFilter[];
-        family: TFilter[];
-        selfMade: TFilter[];
+        deceased: TFilterItem[];
+        dropOff: TFilterItem[];
+        family: TFilterItem[];
+        selfMade: TFilterItem[];
+    };
+}
+
+export interface TFilterCollection {
+    industry: Record< string, TFilter >;
+    citizenship: Record< string, TFilter >;
+    country: Record< string, TFilter >;
+    state: Record< string, TFilter >;
+    gender: { [ K in TGender ]?: TFilter };
+    age: Record< number, TFilter >;
+    maritalStatus: { [ K in TMaritalStatus ]?: TFilter };
+    special: {
+        deceased: TFilter;
+        dropOff: TFilter;
+        family: TFilter;
+        selfMade: TFilter;
     };
 }
