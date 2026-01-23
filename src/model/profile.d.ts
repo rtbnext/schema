f0201d980bd607aeb3e68b2fc03ff4d8c05c9044
komed3 <pk97@outlook.de>
2026-01-23 12:11:10 +0100

    define new TProfileInfo and TProfileBio types

--- src/model/profile.d.ts
+++ src/model/profile.d.ts
@@ -1,60 +1,64 @@
 import { TAnnual, TAsset, TRanking, TRealtime } from '../abstract/assets';
 import { TGender, TIndustry, TMaritalStatus } from '../abstract/const';
-import { TEducation, TImage, TIndex, TLocation, TMetaData, TOrganization, TRelation, TSelfMade, TWiki } from '../abstract/generic';
+import * as G from '../abstract/generic';
 
 export type TProfileIndex = Map< string, TProfileIndexItem >;
 
-export type TProfileIndexItem = TIndex & {
+export type TProfileIndexItem = G.TIndex & {
     aliases: string[];
     image?: string;
     desc?: string;
 }
 
+export interface TProfileInfo {
+    deceased: boolean;
+    family: boolean;
+    dropOff: boolean;
+    embargo?: boolean;
+    name: string;
+    shortName: string;
+    lastName: string;
+    firstName: string;
+    gender: TGender;
+    birthDate?: string;
+    birthPlace?: G.TLocation;
+    citizenship?: string;
+    residence?: G.TLocation;
+    maritalStatus?: TMaritalStatus;
+    children?: number;
+    education?: G.TEducation[];
+    industry: TIndustry;
+    source: string[];
+    selfMade: G.TSelfMade;
+    philanthropyScore?: number;
+    organization?: G.TOrganization;
+}
+
+export interface TProfileBio {
+    cv: string[];
+    facts: string[];
+    quotes: string[];
+}
+
 export interface TProfileData {
     readonly id: string;
     uri: string;
-    info: {
-        deceased: boolean;
-        family: boolean;
-        dropOff: boolean;
-        embargo?: boolean;
-        name: string;
-        shortName: string;
-        lastName: string;
-        firstName: string;
-        gender: TGender;
-        birthDate?: string;
-        birthPlace?: TLocation;
-        citizenship?: string;
-        residence?: TLocation;
-        maritalStatus?: TMaritalStatus;
-        children?: number;
-        education?: TEducation[];
-        industry: TIndustry;
-        source: string[];
-        selfMade: TSelfMade;
-        philanthropyScore?: number;
-        organization?: TOrganization;
-    };
-    bio: {
-        cv: string[];
-        facts: string[];
-        quotes: string[];
-    };
-    related: TRelation[];
-    media: TImage[];
+    info: TProfileInfo;
+    bio: TProfileBio;
+    related: G.TRelation[];
+    media: G.TImage[];
     realtime?: TRealtime;
     ranking: TRanking[];
     annual: TAnnual[];
     assets: TAsset[];
-    wiki?: TWiki;
+    wiki?: G.TWiki;
 }
 
 export type TProfileHistoryItem = [ string, number, number, number, number ];
 
 export type TProfileHistory = TProfileHistoryItem[];
 
-export type TProfile = TMetaData & {
+export type TProfile = G.TMetaData & {
     data: TProfileData;
     history: TProfileHistory;
 }
