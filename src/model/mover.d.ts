11b0696a76aaa9b6a4d548df9732c52dfef3ddfc
komed3 <pk97@outlook.de>
2026-06-16 18:44:43 +0200

    redeclare mover types

---
+++ src/model/mover.d.ts
@@ -0,0 +1,29 @@
+import type { Expand } from 'devtypes/types/util';
+import type { TChangeItem } from '../base/assets';
+import type { TMetaData, TSnapshot } from '../base/generic';
+
+export type TMoverEntry = {
+  readonly uri: string;
+  name: string;
+  value: number;
+};
+
+export type TMoverSubject = {
+  winner: TMoverEntry[];
+  loser: TMoverEntry[];
+};
+
+export type TMoverItem = {
+  total: TChangeItem,
+  networth: TMoverSubject;
+  percent: TMoverSubject;
+};
+
+export type TMover = Expand<
+  TMetaData &
+  TSnapshot &
+  {
+    today: TMoverItem;
+    ytd: TMoverItem;
+  }
+>;
