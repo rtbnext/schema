24047d435956d25db9bb2c2233c195035ba25c5c
komed3 <pk97@outlook.de>
2026-06-17 09:20:03 +0200

    declare filter list

--- src/model/filter.d.ts
+++ src/model/filter.d.ts
@@ -1,4 +1,5 @@
 import type { Expand } from 'devtypes/types/util';
+import type { TGender, TMaritalStatus } from '../base/const';
 import type { TMetaData } from '../base/generic';
 
 export type TFilterItem = {
@@ -11,3 +12,21 @@ export type TFilter = Expand< TMetaData & {
   items: TFilterItem[];
   count: number;
 } >;
+
+export type TFilterSpecialGroup = {
+  deceased: TFilterItem[];
+  dropOff: TFilterItem[];
+  family: TFilterItem[];
+  selfMade: TFilterItem[];
+};
+
+export type TFilterList = {
+  industry: Record< string, TFilterItem[] >;
+  citizenship: Record< string, TFilterItem[] >;
+  country: Record< string, TFilterItem[] >;
+  state: Record< string, TFilterItem[] >;
+  gender: { [ K in TGender ]?: TFilterItem[] };
+  age: Record< number, TFilterItem[] >;
+  maritalStatus: { [ K in TMaritalStatus ]?: TFilterItem[] };
+  special: TFilterSpecialGroup;
+};
