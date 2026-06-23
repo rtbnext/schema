197e86ba5d4bbd2e35b9667fdcc0587d38e42b4c
komed3 <pk97@outlook.de>
2026-06-23 10:14:15 +0200

    add unknown change flag

--- src/base/const.d.ts
+++ src/base/const.d.ts
@@ -21,7 +21,7 @@ export type TAssetType =
   | 'public' | 'private' | 'misc';
 
 export type TChangeFlag =
-  | 'up' | 'down' | 'unchanged' | 'new' | 'returned';
+  | 'up' | 'down' | 'unchanged' | 'new' | 'returned' | 'unknown';
 
 export type TStatsGroup =
   | 'industry' | 'citizenship';
