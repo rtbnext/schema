b499bf68b72db9fa1606e0cd5a42c9d34ccdd692
komed3 <webmaster@komed3.de>
2026-07-03 17:40:10 +0200

    add comments for list types

--- src/model/list.d.ts
+++ src/model/list.d.ts
@@ -13,6 +13,9 @@ import type { TIndex, TMetaData, TSnapshot } from '../base/generic';
 import type { TGenericStats } from './stats';
 
 
+/**
+ * List index entry.
+ */
 export type TListIndexItem = Expand< TIndex & {
   shortName: string;
   desc: string;
@@ -20,29 +23,56 @@ export type TListIndexItem = Expand< TIndex & {
   filters: string[];
 } >;
 
+/**
+ * List index.
+ */
 export type TListIndex = Map< string, TListIndexItem >;
 
+/**
+ * Base list item.
+ */
 export type TListItem = {
   uri: string;
   name: string;
   rank: number;
 };
 
+/**
+ * Snapshot of a ranking list.
+ * 
+ * @template T List item type.
+ */
 export type TListSnapshotData< T extends TListItem = TListItem > = Expand< TSnapshot & {
   items: T[];
   count: number;
   stats: TGenericStats;
 } >;
 
+/**
+ * Ranking list snapshot document.
+ * 
+ * @template T List item type.
+ */
 export type TListSnapshot< T extends TListItem = TListItem > = Expand< TMetaData & TListSnapshotData< T > >;
 
+/**
+ * Collection of ranking list snapshots.
+ * 
+ * @template T Snapshot type.
+ */
 export type TList< T extends TListSnapshot = TListSnapshot > = Record< string, T >;
 
+/**
+ * Collection of all ranking lists and their index.
+ */
 export type TListCollection = {
   list: Record< string, TList >;
   index: TListIndex;
 };
 
+/**
+ * Base person ranking entry.
+ */
 export type TPersonListItem = Expand< TListItem & {
   networth: number;
   industry: TIndustry;
@@ -52,16 +82,28 @@ export type TPersonListItem = Expand< TListItem & {
   citizenship?: string;
 } >;
 
+/**
+ * RTB ranking entry.
+ */
 export type TRTBListItem = Expand< TPersonListItem & TChange & {
   flag: TChangeFlag;
   rankDiff?: number;
 } >;
 
+/**
+ * RTB ranking snapshot document.
+ */
+export type TRTBListSnapshot = TListSnapshot< TRTBListItem >;
+
+/**
+ * Billionaires ranking entry.
+ */
 export type TBillionairesListItem = Expand< TPersonListItem & {
   selfMadeRank?: TSelfMadeRank;
   philanthropyScore?: number;
 } >;
 
+/**
+ * Forbes 400 ranking entry.
+ */
 export type T400ListItem = TBillionairesListItem;
-
-export type TRTBListSnapshot = TListSnapshot< TRTBListItem >;
