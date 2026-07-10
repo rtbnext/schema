05cb2f7041221dd4d6b0abf63caba4dcf0f554a1
komed3 <webmaster@komed3.de>
2026-07-10 09:23:37 +0200

    add JSDoc comments

--- src/model/status.d.ts
+++ src/model/status.d.ts
@@ -1,4 +1,21 @@
+/**
+ * RTBNext Schema V2
+ * Status Types
+ * 
+ * Shared type definitions for the RTBNext status document.
+ * These types define the structure and allowed values for the status
+ * document, which provides information about the health and availability
+ * of RTBNext services.
+ */
+
 import type { Expand } from 'devtypes/types/util';
+import type { TStatusFlag } from '../base/const';
 import type { TMetaData } from '../base/generic';
 
-export type TStatus = Expand< TMetaData & {} >;
+/**
+ * Status document.
+ */
+export type TStatus = Expand< TMetaData & {
+  status: TStatusFlag;
+  services: { [ K in ( 'profiles' | 'lists' | 'movers' | 'filters' | 'statistics' ) ]: TStatusFlag };
+} >;
